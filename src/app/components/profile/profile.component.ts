c1bcd6c4957bd2723e7e00c4dd272ab18f36925d
titaks35 <turkungr35@gmail.com>
2025-10-19 16:49:06 +0700

    fix(profile): apply prettier and -rm spec file.
    1.format code with "prettier" code formatter.
    2.remove spec.ts file.

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -4,8 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-profile',
   standalone: false,
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
-
-}
+export class ProfileComponent {}
