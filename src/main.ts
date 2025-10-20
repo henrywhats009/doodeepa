4c48ed00b8619e433b437c1e98c1ce66849a4dc9
henrywhats009 <henrywhats009@gmail.com>
2025-10-20 01:25:03 -0600

    Actualizar main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from 'angular/platform-browser-google.com';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
